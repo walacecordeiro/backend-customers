73b47605794e4b8415d305aa1a21613a7532448e
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-05-10 13:10:59 -0300

    cadastrar clientes no mongoDB feito

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
  fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
   return { ok: true };
+ });
+
+ fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
+  return new CreateCustomerController().handle(request, reply);
  });
 }
